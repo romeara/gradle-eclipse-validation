8d1ff1c1f9c517393771256f770a0f15d89b3f4b
Ryan O'Meara <romeara@live.com>
2016-07-14 23:32:46 -0400

    Add ability to set/remove validation builder

--- src/main/groovy/com/rsomeara/gradle/eclipse/validation/EclipseValidationPlugin.groovy
+++ src/main/groovy/com/rsomeara/gradle/eclipse/validation/EclipseValidationPlugin.groovy
@@ -17,31 +17,54 @@ package com.rsomeara.gradle.eclipse.validation
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 import org.gradle.plugins.ide.eclipse.EclipsePlugin
+import org.gradle.plugins.ide.eclipse.model.BuildCommand
 import org.gradle.plugins.ide.eclipse.model.EclipseModel
 import org.gradle.util.ConfigureUtil
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import com.rsomeara.gradle.eclipse.validation.model.EclipseValidation
 
 public class EclipseValidationPlugin implements Plugin<Project> {
 
-    //Add validation to EclipseModel via meta-classes. Feels a little hack-ish, but allows the user to define validation within the existing Eclipse scope
-    static{
-        EclipseModel.metaClass.wstValidation = new EclipseValidation()
-        EclipseModel.metaClass.validation = {closure -> ConfigureUtil.configure(closure, wstValidation)}
-    }
+	private static final Logger logger = LoggerFactory.getLogger(EclipseValidationPlugin.class)
 
-    void apply(Project project) {
-        project.task('hello') << { println "Hello from the EclipseValidationPlugin" }
+	//Add validation to EclipseModel via meta-classes. Feels a little hack-ish, but allows the user to define validation within the existing Eclipse scope
+	static{
+		EclipseModel.metaClass.wstValidation = new EclipseValidation()
+		EclipseModel.metaClass.validation = {closure -> ConfigureUtil.configure(closure, wstValidation)}
+	}
 
-        //Requires the Eclipse plug-in to be applied, as it adds elements to it
-        project.pluginManager.apply(EclipsePlugin)
+	void apply(Project project) {
+		project.task('hello') << { println "Hello from the EclipseValidationPlugin" }
 
-        //Add task dependency
-        Task lifecycleTask = project.task('eclipseWstValidation');
-        project.configure(lifecycleTask, {
-            EclipseModel eclipseModel = project.extensions.getByType(EclipseModel)
-            println "Separate Builder: " + eclipseModel.wstValidation.separateBuilder
-        })
-        project.tasks.getByName(EclipsePlugin.ECLIPSE_TASK_NAME).dependsOn(lifecycleTask)
-    }
+		//Requires the Eclipse plug-in to be applied, as it adds elements to it
+		project.pluginManager.apply(EclipsePlugin)
+
+		//Add task dependency
+		configureEclipseProject(project)
+	}
+
+	private void configureEclipseProject(Project project) {
+		project.afterEvaluate{
+			EclipseModel eclipse = project.extensions.getByType(EclipseModel)
+
+			if(!eclipse.wstValidation.separateBuilder){
+				eclipse.project.file.whenMerged{projectFile ->
+					projectFile.buildCommands.removeAll{'org.eclipse.wst.validation.validationbuilder'.equals(it.name)}
+				}
+			}else{
+				boolean found = false
+
+				eclipse.project.buildCommands.forEach{
+					found = found || 'org.eclipse.wst.validation.validationbuilder'.equals(it.name)
+				}
+
+				if(!found){
+					eclipse.project.buildCommands.add(new BuildCommand('org.eclipse.wst.validation.validationbuilder'))
+				}
+			}
+		}
+	}
 }
